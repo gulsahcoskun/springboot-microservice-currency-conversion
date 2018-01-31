1784711532f18c9dce1fb6bcb505e44e4593c7ed
gulsah <gulsahcoskunn@gmail.com>
2018-01-31 22:58:06 +0300

    enableFeignClients added

--- SpringbootMicroserviceCurrencyConversionApplication.java
+++ SpringbootMicroserviceCurrencyConversionApplication.java
@@ -2,8 +2,10 @@ package com.gulsah.project.microservice.currencyconversion.springbootmicroservic
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients("com.gulsah.project.microservice.currencyconversion.springbootmicroservicecurrencyconversion")
 public class SpringbootMicroserviceCurrencyConversionApplication {
 
 	public static void main(String[] args) {
